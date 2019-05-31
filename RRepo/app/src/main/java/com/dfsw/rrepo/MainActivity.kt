7f6a21e87541eeacc3efba53e7b53e9c2ae9bd10
diegof <diegof@inatel.br>
2019-05-31 09:37:54 -0300

    Creted a project for Room Repository

---
+++ RRepo/app/src/main/java/com/dfsw/rrepo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dfsw.rrepo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
